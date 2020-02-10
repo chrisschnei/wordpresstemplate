b72d4bdd28f9fb120e51f17b5dc18d32a9f8ab3b
Christian Schneider <christian.schneider3@gmx.net>
2020-02-10 19:07:32 +0100

    added 404 error page
    Signed-off-by: Christian Schneider <christian.schneider3@gmx.net>

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 404 error page for our theme.
+ * Template Name: 404 error page
+ *
+ * @package Treffpunkt60plusminus
+ * @subpackage Treffpunkt60plusminus
+ * @since Treffpunkt60plusminus 0.0
+ */
+?>
+
+<?php get_header(); ?>
+
+  <div class="container">
+    <div class="jumbotron-right">
+	<a HREF="javascript:goBack()"><h3>Zurück zur vorherigen Seite.</h3></a>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
+
+<script>
+function goBack() { window.history.back() }
+</script>
